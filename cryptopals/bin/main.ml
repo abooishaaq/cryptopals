f033ffffe5fe287eb2cc25faa89b092729e86eca
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-08-27 21:33:18 +0530

    add challenge 4

--- cryptopals/bin/main.ml
+++ cryptopals/bin/main.ml
@@ -3,30 +3,31 @@ open Cryptopals.Bytes_to_hex
 open Cryptopals.Hex_xor
 open Cryptopals.Utils
 
+let score s = 
+  let sc = ref 0.0 in
+  for i = 0 to String.length s - 1 do
+    let c = Char.code s.[i] in
+    if c > 127 then
+      sc := !sc -. 5.0
+    else if c < 32 && not (c = 10 || c = 13) then
+      sc := !sc -. 7.0
+    else if is_alpha c then
+      sc := !sc +. 12.0
+  done;
+  !sc /. float_of_int (String.length s)
+;;
+
 let challenge3 =
   let hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736" in
   let l = String.length hex / 2 in
   let rec repeat str times aux = 
     if times = 0 then aux
     else repeat str (times - 1) (str ^ aux) in
-  let score s = 
-    let sc = ref 0.0 in
-    for i = 0 to String.length s - 1 do
-      let c = Char.code s.[i] in
-      if c > 127 then
-        sc := !sc -. 1.0
-      else if c < 32 && not (c = 10 || c = 13) then
-        sc := !sc -. 5.0
-      else if is_alpha c then
-        sc := !sc +. 1.5
-    done;
-    !sc /. float_of_int (String.length s)
-  in
   let rec aux i acc =
     if i = 256 then acc
     else
       let byte = Char.chr i in
-      let byte_hexed =   repeat (String.of_bytes (bytes_to_hex (Bytes.make 1 byte))) l "" in
+      let byte_hexed = repeat (String.of_bytes (bytes_to_hex (Bytes.make 1 byte))) l "" in
       let xor = hex_xor hex byte_hexed in
       let res = hex_to_bytes (String.of_bytes xor) in
       let score = score (Bytes.to_string res) in
@@ -35,7 +36,32 @@ let challenge3 =
   aux 0 [] |> List.sort (fun (a, _) (b, _) -> compare b a) |> List.hd |> snd |> Bytes.to_string |> print_endline
 ;;
 
-challenge3
+let challenge4 = 
+  let lines = read_file "data/4.txt" in
+  (* select the string with the most english letters and then brute force xor it *)
+  let best = List.fold_left (fun acc line -> 
+      let score = score (Bytes.to_string (hex_to_bytes line)) in
+      if score > fst acc then (score, line) else acc
+    ) (0.0, "") lines in
+  let hex = snd best in
+  let l = String.length hex / 2 in
+  let rec repeat str times aux =
+    if times = 0 then aux
+    else repeat str (times - 1) (str ^ aux) in
+  let rec aux i acc =
+    if i = 256 then acc
+    else
+      let byte = Char.chr i in
+      let byte_hexed = repeat (String.of_bytes (bytes_to_hex (Bytes.make 1 byte))) l "" in
+      let xor = hex_xor hex byte_hexed in
+      let res = hex_to_bytes (String.of_bytes xor) in
+      let score = score (Bytes.to_string res) in
+      aux (i + 1) ((score, res) :: acc);
+  in
+  aux 0 [] |> List.sort (fun (a, _) (b, _) -> compare b a) |> List.hd |> snd |> Bytes.to_string |> print_string
+;;
 
+challenge3;;
+challenge4;;
 
 
--- cryptopals/lib/utils.ml
+++ cryptopals/lib/utils.ml
@@ -9,3 +9,16 @@ let char_list_to_string l =
 let is_alpha code = 
   code >= 65 && code <= 90 || code >= 97 && code <= 122
 ;;
+
+let read_file filename = 
+  let lines = ref [] in
+  let chan = open_in filename in
+  try
+    while true; do
+      lines := input_line chan :: !lines
+    done; !lines
+  with End_of_file ->
+    close_in chan;
+    List.rev !lines 
+;;
+  
