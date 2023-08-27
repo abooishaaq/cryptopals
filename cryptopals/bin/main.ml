c9a0b8d1a786c086adcbba8ee9dbd5ee64d17ede
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-08-27 21:16:33 +0530

    challenge 3

--- cryptopals/bin/main.ml
+++ cryptopals/bin/main.ml
@@ -1,17 +1,41 @@
-open Cryptopals.Hex_to_base64
-open Cryptopals.Bytes_to_base64
+open Cryptopals.Hex_to_bytes
+open Cryptopals.Bytes_to_hex
+open Cryptopals.Hex_xor
+open Cryptopals.Utils
 
-let () =
-  let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d" in
-  let base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" in
-  let result = hex_to_base64 hex in
-  if result = base64 then
-    Printf.printf "OK\n"
-  else
-    Printf.printf "KO\n"
-    ;
-  let res1 = Base64.encode_string "any pleasure." in
-  let res2 = bytes_to_base64 (Bytes.of_string "any pleasure.") in
-  print_endline res1;
-  print_endline res2;
+let challenge3 =
+  let hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736" in
+  let l = String.length hex / 2 in
+  let rec repeat str times aux = 
+    if times = 0 then aux
+    else repeat str (times - 1) (str ^ aux) in
+  let score s = 
+    let sc = ref 0.0 in
+    for i = 0 to String.length s - 1 do
+      let c = Char.code s.[i] in
+      if c > 127 then
+        sc := !sc -. 1.0
+      else if c < 32 && not (c = 10 || c = 13) then
+        sc := !sc -. 5.0
+      else if is_alpha c then
+        sc := !sc +. 1.5
+    done;
+    !sc /. float_of_int (String.length s)
+  in
+  let rec aux i acc =
+    if i = 256 then acc
+    else
+      let byte = Char.chr i in
+      let byte_hexed =   repeat (String.of_bytes (bytes_to_hex (Bytes.make 1 byte))) l "" in
+      let xor = hex_xor hex byte_hexed in
+      let res = hex_to_bytes (String.of_bytes xor) in
+      let score = score (Bytes.to_string res) in
+      aux (i + 1) ((score, res) :: acc);
+  in
+  aux 0 [] |> List.sort (fun (a, _) (b, _) -> compare b a) |> List.hd |> snd |> Bytes.to_string |> print_endline
 ;;
+
+challenge3
+
+
+
--- cryptopals/lib/utils.ml
+++ cryptopals/lib/utils.ml
@@ -5,3 +5,7 @@ let char_list_to_string l =
     | h::t -> aux t (acc ^ (Char.escaped h))
   in aux l ""
 ;;
+
+let is_alpha code = 
+  code >= 65 && code <= 90 || code >= 97 && code <= 122
+;;
